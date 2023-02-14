a47d9f8b3691988340ae8b2b60c7f72dc1aae483
Mayra Malaquias <mayra.malaquias@hotmail.com>
2023-02-13 21:28:41 -0300

    :iphone:

---
+++ script.js
@@ -0,0 +1,14 @@
+//const billInput = document.querySelector("#bill");
+//const peopleInput = document.querySelector("#people");
+//const botaosInput = document.querySelector(".opcoes_porcentagem")
+
+
+//console.log(billInput);
+
+var input = document.querySelector("#bill")
+
+var texto = input.value
+
+console.log(texto)
+
+
